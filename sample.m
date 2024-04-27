5655b6a67975f703b878cde02196d22cbf193d6a
eSqadron <kubam0906@gmail.com>
2024-04-27 10:55:50 +0200

    Scan Statuses named

--- sample.m
+++ sample.m
@@ -6,20 +6,20 @@ v.define_scanner(1, 10, 30, 5, ...
 
 v.start_scan()
 
-while(v.get_status() ~= "5")
+while(v.get_status() ~= "Finished")
     pause(0.1);
-    while(v.get_status() == "2")
+    while(v.get_status() == "Scanning")
         pause(0.1);
     end
 
     status = v.get_status();
 
-    if(status == "6")
-        break
+    if(status == "Finished")
+        break;
     end
     
-    assert(status == "3", status);
-    v.get_point()
+    assert(status == "WaitingForContinuation", status);
+    [yaw, pitch] = v.get_point()
     
     % Here goes measurement
 
